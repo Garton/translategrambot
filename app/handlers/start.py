e8600b155b32dc3e6195c2f27b7dc9e4f394a9a2
BuildTools <asichci@mail.ru>
2025-07-30 23:28:59 +0300

    +help route

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,5 +1,5 @@
 from aiogram import Router, types
-from aiogram.filters import CommandStart
+from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 
 from app.i18n.resources import get_text
@@ -14,5 +14,15 @@ async def cmd_start(msg: types.Message, state: FSMContext):
 
     await msg.answer(
         await get_text(ui_lang, "start"),  # <- await !
+        reply_markup=choose_pair_btn(ui_lang),
+    )
+
+
+@router.message(Command("help"))
+async def cmd_help(msg: types.Message, state: FSMContext):
+    ui_lang = (msg.from_user.language_code or "en")[:2]
+
+    await msg.answer(
+        await get_text(ui_lang, "help"),  # <- await !
         reply_markup=choose_pair_btn(ui_lang),
     )
